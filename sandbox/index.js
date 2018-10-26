fbfdb0661c25efdbf19e69200ab545af0ae4c7a6
Oz Ramos <labofoz@gmail.com>
2018-10-25 17:33:08 -0700

    #3 Adds highlightJS, adds section element, splits out sandbox code

--- sandbox/index.js
+++ sandbox/index.js
@@ -1,21 +1,5 @@
-require('spectre.css')
-require('../assets/style.styl')
-const avatar = require('../assets/avatar/face-001.json')
-
 window.handsfree = new Handsfree({debug: true})
-
-/**
- * Render the avatar
- */
-window.addEventListener('handsfree-injectDebugger', (ev) => {
-  handsfree.faces = avatar
-  handsfree.debug.$canvas.width = 640
-  handsfree.debug.$canvas.height = 480
-  handsfree.debug.$canvas.parentElement.style.display = 'block'
-
-  handsfree.drawFaces()
-  document.querySelector('body > div').style.opacity = '1'
-})
+require('./sandbox.js')
 
 /**
  * Bind to the handsfree-trackFaces event
